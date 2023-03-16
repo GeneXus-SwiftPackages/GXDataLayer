ddf2ae75045b298dde13354316c3c45dede7001a
Build Er <builder@macmini6.local>
2023-03-16 14:54:10 -0300

    Package version: 1.0.0-beta+20230316174639

--- Package.swift
+++ Package.swift
@@ -19,14 +19,14 @@ let package = Package(
 				dependencies: [
 					"GXDataLayer",
 					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.iOS])),
-					.product(name: "GXObjectsModel", package: "GXObjectsModel", condition: .when(platforms: [.tvOS, .watchOS, .iOS])),
+					.product(name: "GXObjectsModel", package: "GXObjectsModel", condition: .when(platforms: [.watchOS, .tvOS, .iOS])),
 					.product(name: "YAJL", package: "YAJL", condition: .when(platforms: [.iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXDataLayer",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXDataLayer-1.0.0-beta+20230316142352.xcframework.zip",
-			checksum: "f7db1201f66876ab644701def7785502c57ee4b5799f735ad45a033a4d3a5020"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXDataLayer-1.0.0-beta+20230316174639.xcframework.zip",
+			checksum: "45764f35dc57ba33d29663e10023f5c2c94f716e34ad86b635908a0137db7d82"
 		)
 	]
 )
