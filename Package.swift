f5422b83002948fa59486c65f941c98e4fd9c227
genexusbot <builder@genexus.com>
2023-05-09 10:21:04 -0300

    Package version: 1.0.0-beta+20230509131236

--- Package.swift
+++ Package.swift
@@ -19,14 +19,14 @@ let package = Package(
 				dependencies: [
 					"GXDataLayer",
 					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.iOS])),
-					.product(name: "GXObjectsModel", package: "GXObjectsModel", condition: .when(platforms: [.iOS, .tvOS, .watchOS])),
+					.product(name: "GXObjectsModel", package: "GXObjectsModel", condition: .when(platforms: [.watchOS, .tvOS, .iOS])),
 					.product(name: "YAJL", package: "YAJL", condition: .when(platforms: [.iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXDataLayer",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXDataLayer-1.0.0-beta+20230509112137.xcframework.zip",
-			checksum: "a8e16633c8a57bdf1d2c5385745ded3f7e69f1e69d27aa5aced3bd3eaf4157d7"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXDataLayer-1.0.0-beta+20230509131236.xcframework.zip",
+			checksum: "649f5320ac9fbf942f147487bcd5cf3a129faa9113709e9a5942a3d4bfc02c75"
 		)
 	]
 )
