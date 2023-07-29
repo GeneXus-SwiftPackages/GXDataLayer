84fc90d6e06f7c98a704cff2c2e04e0e883e4931
genexusbot <builder@genexus.com>
2023-07-29 18:41:31 -0300

    Package version: 1.1.0-rc.1

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXDataLayer",
+	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "GXDataLayer",
+			targets: ["GXDataLayerWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", exact: "1.1.0-rc.1"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXObjectsModel.git", exact: "1.1.0-rc.1"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/YAJL.git", exact: "1.1.0-rc.1")
+	],
+	targets: [
+		.target(name: "GXDataLayerWrapper",
+				dependencies: [
+					"GXDataLayer",
+					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.iOS])),
+					.product(name: "GXObjectsModel", package: "GXObjectsModel", condition: .when(platforms: [.tvOS, .watchOS, .iOS])),
+					.product(name: "YAJL", package: "YAJL", condition: .when(platforms: [.iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXDataLayer",
+			url: "https://pkgs.genexus.dev/iOS/releases/GXDataLayer-1.1.0-rc.1.xcframework.zip",
+			checksum: "2a1f9711cce31c73f9c227cc526fe1d3274ad1c1b6870d17e94c1da974f95f41"
+		)
+	]
+)
