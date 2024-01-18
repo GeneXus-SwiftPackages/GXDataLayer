9d946228603eb59b9450cbeba587ac07f8deea34
genexusbot <builder@genexus.com>
2024-01-18 00:36:41 -0300

    Package version: 1.4.0-rc.21

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
 	name: "GXDataLayer",
-	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	platforms: [.iOS("12.0"), .watchOS("9.0"), .tvOS("12.0")],
 	products: [
 		.library(
 			name: "GXDataLayer",
 			targets: ["GXDataLayerWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXObjectsModel.git", exact: "1.4.0-rc.10")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXObjectsModel.git", exact: "1.4.0-rc.21")
 	],
 	targets: [
 		.target(name: "GXDataLayerWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXDataLayer",
-			url: "https://pkgs.genexus.dev/iOS/preview/GXDataLayer-1.4.0-rc.10.xcframework.zip",
-			checksum: "ff79143fe713809c952e3d885837699ba937b30c1a318b7f2b909a401bb274a5"
+			url: "https://pkgs.genexus.dev/iOS/preview/GXDataLayer-1.4.0-rc.21.xcframework.zip",
+			checksum: "c361bdce82500a589853234f0261883009ab8c8b0216724026cc9326766a65ce"
 		)
 	]
 )
