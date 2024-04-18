e7824f544832b556d78db8a46f9ecf15704dd182
Build Er <builder@macmini6.local>
2024-04-18 12:19:53 -0300

    Package version: 2.0.0-beta.16

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXDataLayerWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXObjectsModel.git", exact: "2.0.0-beta.7")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXObjectsModel.git", exact: "2.0.0-beta.16")
 	],
 	targets: [
 		.target(name: "GXDataLayerWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXDataLayer",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXDataLayer-2.0.0-beta.7.xcframework.zip",
-			checksum: "bc1f2ba2711ba726b1777f7ba693b8ec276d2a92838e80729f61ed1eef85d03e"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXDataLayer-2.0.0-beta.16.xcframework.zip",
+			checksum: "50ede99ec931d02080e12269deeaae4a9e8dbae1fc0cd64ef581890165bdefae"
 		)
 	]
 )
