d2996db0f55ed4964dd65f52e382447a3d644723
The iOS Team @ GeneXus <builder@genexus.com>
2025-08-28 09:18:39 -0400

    Package version: 3.2.0-beta.40

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXDataLayerWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXObjectsModel.git", exact: "3.2.0-beta.39")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXObjectsModel.git", exact: "3.2.0-beta.40")
 	],
 	targets: [
 		.target(name: "GXDataLayerWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXDataLayer",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXDataLayer-3.2.0-beta.39.xcframework.zip",
-			checksum: "492b6086c220520a71c26094c05eabef556f7cfa51e5a009de9f086c51e278fa"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXDataLayer-3.2.0-beta.40.xcframework.zip",
+			checksum: "8490a2a20b3be56d6a6f6b6ac152114f576b0531e8ee83b3b5ff494243f0b45d"
 		)
 	]
 )
